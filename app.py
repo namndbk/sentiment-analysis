e318c68707c8f746fb282a3ee0aca213fcc1e80b
Nguyen Danh Nam <namnd2802@gmail.com>
2020-05-10 22:54:10 +0700

    Update

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ transform = pickle.load(open("models/modelv1/transform", "rb"))
 
 def predict(text):
     text = normalize_text(text.strip())
+    if text == "" or len(text.split()) <= 5:
+        return "Input must have a mininum of 5 words."
     text = transform.transform([text])
     y_pred = model.predict(text)
     return y_pred[-1]
@@ -50,10 +52,5 @@ def api():
     return jsonify(message)
 
 
-# if __name__ == "__main__":
-#     model = pickle.load(open("models/modelv1/svm", "rb"))
-#     transform = pickle.load(open("models/modelv1/transform", "rb"))
-#     app.run()
-
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=os.environ['PORT'])
+	app.run(host="0.0.0.0")
